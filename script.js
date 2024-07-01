58e6e465842f6035d22c316d8c579d9287133aca
Marcelleevelyn <marcelle.evelyn27@gmail.com>
2024-06-30 21:01:36 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+//código do botão enviar do fomrulário;
+const btn = document.getElementsByClassName('btn');
+btn.addEventListener('click', botaoAcionado);
+function botaoAcionado(){
+    window.alert('Parabéns! Você se inscreveu na nossa newsletter!')
+}
